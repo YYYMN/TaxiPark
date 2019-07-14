f0e17673d344b6b0fe45e102b2a3daba644d9b80
Mikhail Novikau <misha_nov87@mail.ru>
2019-07-14 12:28:01 +0300

    fix DBDump

--- src/main/resources/dbdump/DBDump.sql
+++ src/main/resources/dbdump/DBDump.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS `driver`;
 SET character_set_client = utf8mb4;
 CREATE TABLE `driver`
 (
-   id                    int(11) NOT NULL AUTO_INCREMENT,
+  id                    int(11) NOT NULL AUTO_INCREMENT,
   `surname`              varchar(255) DEFAULT NULL,
   `name`                 varchar(255) DEFAULT NULL,
   `patronymic`           varchar(255) DEFAULT NULL,
@@ -15,29 +15,19 @@ CREATE TABLE `driver`
   `address`              varchar(255) DEFAULT NULL,
   `drive_license_number` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE = MyISAM
-  AUTO_INCREMENT = 30
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+) ENGINE = MyISAM;
 
 
 LOCK TABLES `driver` WRITE;
 /*!40000 ALTER TABLE `driver`
   DISABLE KEYS */;
 INSERT INTO `driver`
-VALUES (1, 'Гладкевич', 'Иван', 'Иванович', '1998-07-09', 'М', 'г. Минск ', '15544789'),
-       (2, 'Петров', 'Денис', 'Александрович', '1996-04-15', 'М', 'г. Минск', '26331548'),
-       (3, 'Жнай', 'Максим', 'Викторович', '1969-04-22', 'М', 'г. Витебск', '48115478'),
-       (4, 'Марков', 'Александр', 'Николаевич', '1979-11-05', 'М', 'г. Витебск', '66841125'),
-       (5, 'Панин', 'Семен', 'Владимирович', '1984-10-25', 'М', 'г. Минск', '34117856'),
-       (6, 'Акулич', 'Александра', 'Семеновна', '1996-04-11', 'Ж', 'г. Минск', '2211004'),
-       (7, 'Макаров', 'Артем', 'Артемьевич', '1990-09-01', 'М', 'г. Брест', '10455646'),
-       (8, 'Хвалей', 'Константин', 'Михайлович', '1994-09-01', 'М', 'г. Дзержинск', '01546845'),
-       (9, 'Иванов', 'Сергей', 'Петрович', '1984-09-24', 'М', 'г. Бобруйск', '54684651'),
-       (10, 'Шагинова', 'Екатерина', 'Игоревна', '1998-03-08', 'Ж', 'г. Минск', '44789546'),
-       (11, 'Денисов', 'Сергей', 'Владимирович', '1988-07-15', 'М', 'г. Минск', '422315667');
+VALUES (1, 'Markov', 'Ivan', 'Ivanovich', '1998-07-09', 'M', 'Minsk', '15544789'),
+       (2, 'Petrov', 'Denis', 'Alekseevich', '1996-04-15', 'M', 'Minsk', '26331548'),
+       (3, 'Jnai', 'Maksim', 'Viktorovich', '1969-04-22', 'M', 'Vitebsk', '48115478'),
+       (4, 'Akulich', 'Aleksandr', 'Nikolaevich', '1979-11-05', 'M', 'Vitebsk', '66841125'),
+       (5, 'Petrova', 'Anna', 'Semenovna','1987-10-15', 'F', 'Minsk', '125554687' ),
+       (6, 'Panin', 'Semen', 'Vladimirovich', '1984-10-25', 'M', 'Minsk', '34117856');
 /*!40000 ALTER TABLE `driver`
   ENABLE KEYS */;
 UNLOCK TABLES;
